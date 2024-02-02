de2732bbd27c7e228f691bb2f6cf77a6881eb34c
Nenad05 <markoenandovic2005@gmail.com>
2024-02-02 22:58:31 +0100

    changed project structure, added cors-prxy, added mozzart basic api route...

---
+++ backend/Bookmakers/SubGameOddsStruct.js
@@ -0,0 +1,19 @@
+const subGameOddsStruct = {
+    tuples: [
+        {"1": null,"x": null, "2": null},
+        {"1x": null, "2": null},
+        {"12": null, "x": null},
+        {"1": null, "x2": null},
+        {"0-1": null, "2+": null},
+        {"0-2": null, "3+": null},
+        {"0-3": null, "4+": null},
+        {"0-4": null, "5+": null},
+        {"fh1": null,"fhx": null, "fh2": null},
+        {"fh1x": null, "fh2": null},
+        {"fh12": null, "fhx": null},
+        {"fh1": null, "fhx2": null},
+        {"fh0-1": null, "fh2+": null},
+        {"sh1": null,"shx": null, "sh2": null},
+        {"sh0-1": null, "sh2+": null},
+    ]
+}
