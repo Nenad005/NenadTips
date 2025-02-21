7bc5373b61d72a22bfdee11a5737db10a5369c48
Nenad005 <markonenadovic2005@gmail.com>
2025-02-21 16:51:00 +0100

    implementet mozzart api requests for one page of matches

--- scraper/bookamkers/MozzartBet/mozzart.py
+++ scraper/bookamkers/MozzartBet/mozzart.py
@@ -1,5 +1,12 @@
 # from bookmaker import Bookmaker
 import requests
+import json
+
+PROXY_ADDRESS = 'http://localhost:8000/'
+PROXY_HEADER = {
+    "X-Requested-With": "XMLHttpRequest",
+    "medium": "WEB"
+}
 
 class MozzartBet:
     def __init__(self):
@@ -8,23 +15,47 @@ class MozzartBet:
 
     def start_session(self):
         self.session = requests.Session()
-        response = self.session.get("https://www.mozzartbet.com")
-        print(response.status_code)
+
+    def get_subgame_ids(self):
+        with open("mozzart_football_mapping.json", "r") as file:
+            data = json.load(file)
+        
+        subgame_ids = []
+
+        def collect_values(obj):
+            if isinstance(obj, dict):
+                for value in obj.values():
+                    collect_values(value)
+            elif isinstance(obj, list):
+                for item in obj:
+                    collect_values(item)
+            elif obj is not None:
+                subgame_ids.append(obj)
+
+        collect_values(data)
+        return subgame_ids
+
+    def save_content(self, content, filename):
+        with open(filename, "w") as file:
+            json.dump(content, file, indent=4)
 
     def make_dummy_post_request(self):
-        url = "https://www.mozzartbet.com/betting/matches"
+        url = f"{PROXY_ADDRESS}https://www.mozzartbet.com/betting/matches"
         data = {
-            "date": "three_days",
+            "date": "all_days",
             "sort": "bycompetition",
             "currentPage": 0,
             "pageSize": 100,
             "sportId": 1,
             "competitionIds": [],
+            "subgameIds": self.get_subgame_ids(),
             "search": "",
             "matchTypeId": 0
         }
-        response = self.session.post(url, data=data)
-        print(response.status_code, response.content)
+        response = self.session.post(url, data=data, headers=PROXY_HEADER)
+
+        self.save_content(response.json(), "mozzart.json")
+        print(response.status_code)
 
     def close_session(self):
         if self.session:
